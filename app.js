1ff491aa1360cb8d48ae31349fe68bfbfa9d5acf
off365 <141884715+off365@users.noreply.github.com>
2023-08-09 18:29:36 -0600

    update

---
+++ app.js
@@ -0,0 +1,22 @@
+const email = document.querySelector(".email");
+const sign = document.querySelector(".sign");
+const cancle = document.querySelector(".fa-xmark")
+
+sign.addEventListener("click" ,function(){
+    email.classList.add("open")
+})
+
+cancle.addEventListener("click", function(){
+    email.classList.remove("open")
+})
+
+const bars = document.querySelector(".fa-bars");
+const links = document.querySelector(".links");
+
+bars.addEventListener("click",function(){
+    if(links.classList.contains("drop")){
+        links.classList.remove("drop")
+    }else{
+        links.classList.add("drop")
+    }
+})
